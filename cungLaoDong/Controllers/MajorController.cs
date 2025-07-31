ec28fb41d82ee660aa0bafdae4d2a23a716e4a0f
Minh Tuấn Nguyễn <84488169+Minhtuan622@users.noreply.github.com>
2025-07-31 22:30:35 +0700

    Add CRUD for major

---
+++ cungLaoDong/Controllers/MajorController.cs
@@ -0,0 +1,90 @@
+using cungLaoDong.Data;
+using cungLaoDong.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace cungLaoDong.Controllers;
+
+public class MajorController(ApplicationDbContext context) : Controller
+{
+    // GET
+    public async Task<IActionResult> Index()
+    {
+        return View(model: await context.MajorModels.ToListAsync());
+    }
+
+    // GET
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Add(MajorModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model: model);
+        }
+
+        await context.AddAsync(entity: model);
+        await context.SaveChangesAsync();
+        return RedirectToAction(actionName: nameof(Index));
+    }
+
+    // GET
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var data = await context.FindAsync<MajorModel>(keyValues: id);
+        if (data is null)
+        {
+            return NotFound();
+        }
+
+        return View(model: data);
+    }
+
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, MajorModel model)
+    {
+        var data = await context.FindAsync<MajorModel>(keyValues: id);
+        if (id != data!.Id)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(model: model);
+        }
+        
+        data.Name = model.Name;
+        context.Update(entity: data);
+        await context.SaveChangesAsync();
+        return RedirectToAction(actionName: nameof(Index));
+    }
+    
+    [HttpPost, ActionName(name: "Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var data = await context.MajorModels.FindAsync(keyValues: id);
+        if (data is not null)
+        {
+            context.MajorModels.Remove(entity: data);
+        }
+
+        await context.SaveChangesAsync();
+        return RedirectToAction(actionName: nameof(Index));
+    }
+
+}
